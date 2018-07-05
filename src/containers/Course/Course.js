e2037e9fa03e93c234556031f5a48132e180425e
Thiago Morais <thiago.morais@pingwlt.com>
2018-07-05 12:16:54 +0100

    first commit

---
+++ src/containers/Course/Course.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Course extends Component {
+    state= {
+        courseTitle: ''
+    }
+
+    componentDidUpdate(){
+        this.parsequeryParams();
+    }
+
+    componentDidMount(){
+        this.parsequeryParams();
+    }
+
+    parsequeryParams(){
+        console.log(this.props);
+        const query = new URLSearchParams(this.props.location.search);
+        for(let param of query.entries())
+        {
+            if(this.state.courseTitle !== param[1]){
+                this.setState({
+                    courseTitle: param[1]
+                });
+            }
+            //console.log(param);
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>{this.state.courseTitle}</h1>
+                <p>You selected the Course with ID: {this.props.match.params.courseId}</p>
+            </div>
+        );
+    }
+}
+
+export default Course;
